821a41053ceaa873515373a076963308526a5018
fanshicomic <fanshicomic@gmail.com>
2015-11-09 01:12:47 +0800

    add cover ainimation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+var animate;
+function animateCover() {
+	var round = 22;
+	var total = round;
+	var speed = 10;
+	var time = 50;
+	animate = setInterval(function(){
+		moveAlice(speed, total, round--);
+	}, time);
+}
+	
+
+function moveAlice(speed, total, round) {
+	var top = parseInt($("#alice").css("top"));
+	$("#alice").css("top", top + speed + (total - round) / 3);
+	console.log(speed + total - round);
+	if (round < 0) {
+		clearInterval(animate);
+		window.location.href = "alice_2.html";
+	}
+}
+
 function visitRight() {
 	localStorage.setItem('rightVisited', true);
 	window.location.href = "alice_5.html";
