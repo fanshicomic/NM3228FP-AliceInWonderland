e3acf08c6bb54e2ae294a069efcbc42d8b2ba2ca
fanshicomic <fanshicomic@gmail.com>
2015-11-09 10:07:53 +0800

    done with cat

--- js/script.js
+++ js/script.js
@@ -64,34 +64,8 @@ function visitCat() {
 	window.location.href = "alice_c1.html";
 }
 
-function visitLeft() {
-	if (localStorage.getItem('rightVisited') == 'true') {
-		window.location.href = "alice_4_true.html";
-	} else {
-		window.location.href = "alice_4.html";
-	}
-}
-
 function resetGame() {
-	localStorage.setItem('rightVisited', false);
-}
-
-function leftdoor_lightup() {
-	var src = "_images/Alice_left_door.jpg";
-	$(".left").attr("src",src);
-}
-
-function rightdoor_lightup() {
-	var src = "_images/Alice_right_door.jpg";
-	$(".right").attr("src",src);
-}
-
-function leftdoor_lightoff() {
-	var src = "_images/Alice_left_door_dark.jpg";
-	$(".left").attr("src",src);
-}
-
-function rightdoor_lightoff() {
-	var src = "_images/Alice_right_door_dark.jpg";
-	$(".right").attr("src",src);
+	localStorage.setItem('catVisited', false);
+	localStorage.setItem('hatterVisited', false);
+	localStorage.setItem('rabbitVisited', false);
 }
